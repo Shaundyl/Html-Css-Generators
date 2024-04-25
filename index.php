5f683c34dff4fe523b1d4567cba5ccbd3c4fcf5d
angel <68887496+channey575@users.noreply.github.com>
2024-04-25 22:57:26 +0800

    Update index.php

--- index.php
+++ index.php
@@ -12,6 +12,10 @@ $html = $htmlGenerator->addTitle('Generated HTML')
                      ->addDiv('container', '<h1>Hello, world!</h1><p>This is a paragraph.</p>')
                      ->addTable(['border' => '1'], [['Name', 'Age'], ['John', '30'], ['Jane', '25']])
                      ->addH1('text-align:center', 'color: blue; font-size: 24px;')
+                     ->addLink('https://example.com', 'Example', ['class' => 'example-link', 'target' => '_blank'])
+                     ->addHeader(1, 'This is an h1 header', ['class' => 'header1'])
+                     ->addHeader(2, 'This is an h2 header', ['id' => 'header2'])
+                     ->addHeader(3, 'This is an h3 header', ['class' => 'header3'])
                      ->closeBody()
                      ->generate();
 
