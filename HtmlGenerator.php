58e30ae2c0b9e7bca50a3e0c439868b47d455656
Shaundyl <163128967+Shaundyl@users.noreply.github.com>
2024-05-08 09:21:55 +0800

    Update HtmlGenerator.php
    Naa nay mga ul og li diri sa navigation bar pani

--- HtmlGenerator.php
+++ HtmlGenerator.php
@@ -2,6 +2,7 @@
 
 namespace MyNamespace;
 
+
 class HtmlGenerator
 {
     protected $html = '';
@@ -10,6 +11,59 @@ class HtmlGenerator
     {
         $this->html .= "<!DOCTYPE html>\n<html>\n<head>\n";
     }
+
+    public function openDiv($class = '', $id = '', $content = '')
+    {
+        $div = "<div";
+        if (!empty($class)) {
+            $div .= " class=\"$class\"";
+        }
+        if (!empty($id)) {
+            $div .= " id=\"$id\"";
+        }
+        $div .= ">\n";
+        $this->html .= $div; // Append to $this->html
+        return $this; // Return $this to allow method chaining
+    }
+    
+
+
+
+    public function closeDiv()
+    {
+        $this->html .= "</div>\n";
+        return $this;
+    }
+    
+//     public function addLink($url, $text, $attributes = [])
+// {
+//     $link = '<a href="' . htmlspecialchars($url) . '"';
+//     foreach ($attributes as $key => $value) {
+//         $link .= ' ' . htmlspecialchars($key) . '="' . htmlspecialchars($value) . '"';
+//     }
+//     $link .= '>' . htmlspecialchars($text) . '</a>';
+//     $this->html .= $link . "\n";
+//     return $this;
+// }
+
+public function addLink($href, $content, $attributes = []) {
+    // Convert the content to HTML entities, but keep the angle brackets
+    $content = htmlentities($content, ENT_NOQUOTES, 'UTF-8', false);
+    $content = str_replace(['&lt;', '&gt;'], ['<', '>'], $content);
+
+    // Start building the link
+    $this->html .= '<a href="' . $href . '"';
+
+    // Add any attributes
+    foreach ($attributes as $key => $value) {
+        $this->html .= ' ' . $key . '="' . htmlspecialchars($value, ENT_QUOTES, 'UTF-8') . '"';
+    }
+
+    // Add the content and close the tag
+    $this->html .= '>' . $content . '</a>';
+
+    return $this;
+}
 
     // Method to add a title to the document
     public function addTitle($title)
@@ -46,6 +100,16 @@ class HtmlGenerator
         return $this;
     }
 
+    // public function openDiv($class = '', $content = ''){
+    //     $this->html .= "<div class=\"$class\"> $content\n";
+    //     return $this;
+    // }
+
+    // public function closeDiv(){
+    //     $this->html .= "</div>";
+    //     return $this;
+    // }
+    
     // Method to add a div element
     public function addDiv($class = '', $content = '')
     {
@@ -53,6 +117,7 @@ class HtmlGenerator
         return $this;
     }
 
+    
     // Method to add a table element
     public function addTable($attributes = [], $rows = [])
     {
@@ -85,16 +150,6 @@ class HtmlGenerator
         echo $this->html;
     }
 
-    public function addLink($url, $text, $attributes = [])
-    {
-        $link = '<a href="' . htmlspecialchars($url) . '"';
-        foreach ($attributes as $key => $value) {
-            $link .= ' ' . htmlspecialchars($key) . '="' . htmlspecialchars($value) . '"';
-        }
-        $link .= '>' . htmlspecialchars($text) . '</a>';
-        $this->html .= $link . "\n";
-        return $this;
-    }
 
     public function addHeader($level, $text, $attributes = [])
     {
@@ -108,15 +163,91 @@ class HtmlGenerator
         return $this;
     }
 
-    public function addButton($text, $attributes = [])
-    {
-        $button = '<button';
+    // public function addButton($text, $attributes = [])
+    // {
+    //     $button = '<button';
+    //     foreach ($attributes as $key => $value) {
+    //         $button .= ' ' . htmlspecialchars($key) . '="' . htmlspecialchars($value) . '"';
+    //     }
+    //     $button .= '>' . htmlspecialchars($text) . '</button>';
+    //     $this->html .= $button . "\n";
+    //     return $this;
+    // }
+
+    public function addButton($content, $attributes = []) {
+        // Start building the button
+        $this->html .= '<button';
+
+        // Add any attributes
         foreach ($attributes as $key => $value) {
-            $button .= ' ' . htmlspecialchars($key) . '="' . htmlspecialchars($value) . '"';
+            $this->html .= ' ' . $key . '="' . htmlspecialchars($value, ENT_QUOTES, 'UTF-8') . '"';
         }
-        $button .= '>' . htmlspecialchars($text) . '</button>';
-        $this->html .= $button . "\n";
+
+        // Add the content and close the tag
+        $this->html .= '>' . $content . '</button>';
+
+        return $this;
+    }
+
+    public function addImg($src, $alt = '', $attributes = [])
+    {
+        $this->html .= "<img src=\"" . htmlspecialchars($src) . "\" alt=\"" . htmlspecialchars($alt) . "\"";
+        foreach ($attributes as $key => $value) {
+            $this->html .= " " . htmlspecialchars($key) . '="' . htmlspecialchars($value) . '"';
+        }
+        $this->html .= ">\n";
+        return $this;
+    }
+
+    public function addParagraph($text, $class = '') {
+        $paragraph = "<p";
+        if (!empty($class)) {
+            $paragraph .= " class=\"$class\"";
+        }
+        $paragraph .= ">$text</p>\n";
+        $this->html .= $paragraph; // Append to $this->html
+        return $this; // Return $this to allow method chaining
+    }
+
+    public function addNav()
+    {
+        $this->html .= "<nav";
+        $this->html .= " class=\"navbar navbar-expand-lg navbar-light bg-light\"";
+        $this->html .= ">\n";
+        return $this;
+    }
+
+    public function openNav($classes = '') {
+        $this->html .= '<nav class="' . $classes . '">';
+        return $this;
+    }
+
+    public function closeNav() {
+        $this->html .= '</nav>';
+        return $this;
+    }
+
+    public function openUl($classes = '') {
+        $this->html .= '<ul class="' . $classes . '">';
+        return $this;
+    }
+
+    public function closeUl() {
+        $this->html .= '</ul>';
+        return $this;
+    }
+
+    public function openLi($classes = '') {
+        $this->html .= '<li class="' . $classes . '">';
+        return $this;
+    }
+
+    public function closeLi() {
+        $this->html .= '</li>';
         return $this;
     }
 
 }
+
+
+
