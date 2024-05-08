3d3f6f4b24a5004c6cb9725ebbdb14529f952154
Shaundyl <163128967+Shaundyl@users.noreply.github.com>
2024-05-08 09:25:05 +0800

    Navigation Pani

--- index.php
+++ index.php
@@ -5,21 +5,47 @@ use MyNamespace\HtmlGenerator;
 
 $htmlGenerator = new HtmlGenerator();
 
-$html = $htmlGenerator->addTitle('Generated HTML')
-                     ->addCss('styles.css')
-                     ->addMeta('description', 'Generated HTML document')
-                     ->openBody()
-                     ->addDiv('container', '<h1>Hello, world!</h1><p>This is a paragraph.</p>')
-                     ->addTable(['border' => '1'], [['Name', 'Age'], ['John', '30'], ['Jane', '25']])
-                     ->addH1('text-align:center', 'color: blue; font-size: 24px;')
-                     ->addLink('https://example.com', 'Example', ['class' => 'example-link', 'target' => '_blank'])
-                     ->addHeader(1, 'This is an h1 header', ['class' => 'header1'])
-                     ->addHeader(2, 'This is an h2 header', ['id' => 'header2'])
-                     ->addHeader(3, 'This is an h3 header', ['class' => 'header3'])
-                     ->addButton('Click me', ['class' => 'btn', 'id' => 'btn1'])
-                     ->addButton('Submit', ['class' => 'btn btn-primary', 'type' => 'submit'])
-                     ->closeBody()
-                     ->generate();
+// Start the body
+$htmlGenerator->openBody();
+
+// Header section
+$htmlGenerator->openDiv('header_section')
+    ->openDiv('container-fluid')
+        ->openNav('navbar navbar-expand-lg navbar-light bg-light')
+            ->openDiv('logo')
+            ->addLink('index.html', '<img src="images/logo.png" alt="Logo">', ['class' => 'logo-link'])
+            ->closeDiv()
+            ->addButton('<span class="navbar-toggler-icon"></span>', ['class' => 'navbar-toggler', 'type' => 'button', 'data-toggle' => 'collapse', 'data-target' => '#navbarNav', 'aria-controls' => 'navbarNav', 'aria-expanded' => 'false', 'aria-label' => 'Toggle navigation'])
+            ->openDiv('collapse navbar-collapse', 'navbarNav')
+                ->openUl('navbar-nav ml-auto')
+                    ->openLi('nav-item active')
+                        ->addLink('index.html', 'Home', ['class' => 'nav-link'])
+                    ->closeLi()
+                    ->openLi('nav-item')
+                        ->addLink('about.html', 'About Us', ['class' => 'nav-link'])
+                    ->closeLi()
+                    ->openLi('nav-item')
+                        ->addLink('gallery.html', 'Gallery', ['class' => 'nav-link'])
+                    ->closeLi()
+                    ->openLi('nav-item')
+                        ->addLink('services.html', 'Services', ['class' => 'nav-link'])
+                    ->closeLi()
+                    ->openLi('nav-item')
+                        ->addLink('contact.html', 'Contact Us', ['class' => 'nav-link'])
+                    ->closeLi()
+                    ->openLi('nav-item')
+                        ->addLink('#', '<i class="fa fa-search" aria-hidden="true"></i>', ['class' => 'nav-link'])
+                    ->closeLi()
+                ->closeUl()
+            ->closeDiv()
+        ->closeNav()
+    ->closeDiv();
+
+// Close the body
+$htmlGenerator->closeBody();
+
+// Generate the final HTML string
+$html = $htmlGenerator->generate();
 
 $file = fopen('generated_html.html', 'w');
 fwrite($file, $html);
